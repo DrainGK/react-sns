65cbabfa798cd27caa56813f9a489175802fd227
ロペズ ギヨム <guillaumelopezgk@hotmail.com>
2023-04-24 17:33:48 +0900

    Project done

--- frontend/src/scenes/homePage/HomePage.jsx
+++ frontend/src/scenes/homePage/HomePage.jsx
@@ -4,6 +4,9 @@ import NavBar from 'scenes/navbar/NavBar';
 import { useSelector } from 'react-redux';
 import UserWidget from 'scenes/widget/UserWidget';
 import MyPostWidget from 'scenes/widget/MyPostWidget';
+import AdvertWidget from 'scenes/widget/AdvertWidget';
+import FriendListWidget from 'scenes/widget/FriendListWidget';
+import PostsWidget from 'scenes/widget/PostsWidget';
 
 const HomePage = () => {
     const isNonMobileScreens = useMediaQuery("(min-width:1000px)");
@@ -26,10 +29,13 @@ const HomePage = () => {
                     mt={isNonMobileScreens ? undefined : "2rem"}
                 >
                     <MyPostWidget picturePath={picturePath} />
+                    <PostsWidget userId={_id} />
                 </Box>
                     {isNonMobileScreens &&(
                         <Box flexBasis="26%">
-
+                            <AdvertWidget />
+                            <Box m="2rem 0" />
+                            <FriendListWidget userId={_id} />
                         </Box>
                     )}
                
